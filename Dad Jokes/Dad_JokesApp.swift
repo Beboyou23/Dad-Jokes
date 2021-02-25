5c2dfe717a760f9927090680801cd96a8f428f41
Jordan Rarrat <Jrarrat@bsd220.org>
2021-02-25 10:20:15 -0600

    Initial Commit

---
+++ Dad Jokes/Dad_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dad_JokesApp.swift
+//  Dad Jokes
+//
+//  Created by Student on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Dad_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
